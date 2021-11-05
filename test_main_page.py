60718f5d0def131facf609b23816dd05019743d5
Kazyabra <psihound@gmail.com>
2021-11-05 20:29:36 +0300

    В test_guest_can_go_login_page добавлен запуск всех тестов login_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -3,6 +3,7 @@
 import pytest
 from .pages.main_page import MainPage
 from .pages.locators import MainPageLocators
+from .pages.login_page import LoginPage
 
 
 def test_guest_should_see_login_link(browser):
@@ -16,11 +17,14 @@ def test_guest_should_see_login_link(browser):
 
 def test_guest_can_go_to_login_page(browser):
     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page = MainPage(browser, MainPageLocators.MAIN_PAGE_LINK)
+    main_page = MainPage(browser, MainPageLocators.MAIN_PAGE_LINK)
     # открываем страницу
-    page.open()
+    main_page.open()
     # выполняем метод страницы - переходим на страницу логина
-    page.go_to_login_page()
+    main_page.go_to_login_page()
+    # инициализируем Page Object, передаем в конструктор экземпляр драйвера и текущий url адрес
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 if __name__ == '__main__':
